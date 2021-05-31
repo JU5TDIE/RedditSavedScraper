90cab5fca4af1af74bb6a2991e808f5e52dae0cc
JU5TDIE <justdie4444@gmail.com>
2021-05-31 11:53:27 +0900

    check_auth - main.py
    Check auth by calling reddit.user.me.
    
    OAuthException: wrong username and password
    ResponseException: wrong client id and secret

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from gallery_dl.job import DownloadJob
 from prawcore.exceptions import OAuthException
 from prawcore.exceptions import ResponseException
 
+def check_auth():
+    try:
+        print('Logined as %s' % reddit.user.me())
+    except OAuthException:
+        print('Wrong Username and Password.')
+        click.pause()
+        sys.exit()
+    except ResponseException:
+        print('Wrong Client ID and Secret')
+        click.pause()
+        sys.exit()
+
 if __name__ == '__main__':
     with open('user.json') as f:
         data = json.load(f)
