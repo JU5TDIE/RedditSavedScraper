4b702d606b540086764ea0859ce9f1b7246a9f65
JU5TDIE <justdie4444@gmail.com>
2021-05-31 11:45:20 +0900

    user.json - main.py
    Read user.json and get client_id, client_secret, user_agent, username, password from user.json

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import click
 from gallery_dl.job import DownloadJob
 from prawcore.exceptions import OAuthException
 from prawcore.exceptions import ResponseException
+
+if __name__ == '__main__':
+    with open('user.json') as f:
+        data = json.load(f)
+
+    reddit = praw.Reddit(
+        client_id=data['client_id'],
+        client_secret=data['client_secret'],
+        user_agent=data['user_agent'],
+        username=data['username'],
+        password=data['password']
+    )
